0503d7f45e68ee3b4e9a301726b69035eddef2f9
shamanth <ushamanth771@gmail.com>
2022-09-29 12:01:01 +0530

    react-hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React,{useState} from "react"
+
+const App=()=>{
+    const [gold, setGold]  = useState(0)
+    console.log("hello")
+
+return(
+    <div>
+        <h1>{gold}</h1>
+        <button onClick={()=>(gold<=9? setGold(gold+1):"")}>Increase Gold</button>
+        <button onClick={()=>(gold>=-9? setGold(gold-1):"")}>Decrease Gold</button>
+        <button onClick={()=>{setGold(0)}}>Reset Gold</button>
+
+
     </div>
-  );
+)
 }
-
-export default App;
+export default App
